7992a737af11b1dba7a3295b9da7c01900f2042a
Marcelo Ampuero <marcelo.ampuero@gmail.com>
2018-06-14 15:15:08 -0400

    basic project template

---
+++ src/test/java/io/twentythreepeople/training/tdd4dummies/AplicationTests.java
@@ -0,0 +1,14 @@
+package io.twentythreepeople.training.tdd4dummies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AplicationTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/io/twentythreepeople/training/tdd4dummies/Config.java
@@ -0,0 +1,8 @@
+package io.twentythreepeople.training.tdd4dummies;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+}
